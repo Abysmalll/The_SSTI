d279fa1b3a9d4f774211c912beedbd26bba1b239
Abysmalll <hozhiyi02@gmail.com>
2025-01-22 12:35:16 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -26,4 +26,4 @@ def index():
     return render_template('index.html', input=user_input, output=rendered_output)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=80, debug=False)
